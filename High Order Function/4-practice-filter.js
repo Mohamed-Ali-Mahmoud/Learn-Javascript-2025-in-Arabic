dfe19d977607f994d0f781ae092b15d30264f6cc
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-01 22:13:46 +0200

    add numbers to files

---
+++ High Order Function/4-practice-filter.js
@@ -0,0 +1,21 @@
+// Filter Words More Than 4 Characters
+let sentence = "I Love Foood Code Too Playing Much";
+
+let smallWords = sentence.split(" ").filter((ele) => {
+  return ele.length <= 4;
+});
+console.log(smallWords);
+
+// Filter Strings + Multiply
+let mix = "A13BS2ZX";
+
+let num = mix
+  .split("")
+  .filter((el) => {
+    return !isNaN(parseInt(el)) * 2; // if is not string return it and multiply by 2
+  })
+  .map((ele) => {
+    return ele * 2;
+  });
+
+console.log(num);
