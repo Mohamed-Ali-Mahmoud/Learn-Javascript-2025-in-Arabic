7c588fa0d712e18220f9d48fff930f137f6ba00b
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-01 21:04:01 +0200

    add a filter.js file

---
+++ High Order Function/filter.js
@@ -0,0 +1,27 @@
+/*
+  - Filter
+  --- method creates a new array
+  --- with all elements that pass the test implemented by the provided function.
+
+  Syntax filter(callBackFunction(Element, Index, Array) { }, thisArg)
+  - Element => The current element being processed in the array.
+  - Index => The index of the current element being processed in the array.
+  - Array => The Current Array
+*/
+
+// Get Friends With Name Starts With A
+let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
+
+let filterdFreinds = friends.filter((ele) => {
+  return ele.startsWith("A");
+});
+
+console.log(filterdFreinds);
+
+// Get Even Numbers Only
+let numbers = [11, 20, 2, 5, 17, 10];
+
+let evenNumbers = numbers.filter((ele) => {
+  return ele % 2 === 0;
+});
+console.log(evenNumbers);
